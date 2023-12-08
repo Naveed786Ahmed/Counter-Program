82c58fe9dc8454917b32cd149a231db10133a158
Naveed786Ahmed <138322439+Naveed786Ahmed@users.noreply.github.com>
2023-12-08 15:27:02 +0500

    Counter Program

---
+++ app.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+document.getElementById("increase").onclick = function(){
+    count++;
+    document.getElementById("counter").innerHTML = count;
+}
+
+document.getElementById("reset").onclick = function(){
+    count = 0;
+    document.getElementById("counter").innerHTML = count;
+}
+
+document.getElementById("decrease").onclick = function(){
+    count--;
+    document.getElementById("counter").innerHTML = count;
+}
